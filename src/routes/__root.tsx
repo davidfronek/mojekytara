93bf618bb337e84f5eea7705bc753576827c62d1
David Froněk <davidfronek.cz@gmail.com>
2026-05-18 14:28:58 +0200

    preload CSS, fix font display

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -26,7 +26,8 @@ export const Route = createRootRoute({
     links: [
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=optional' },
+      { rel: 'preload', href: appCss, as: 'style' },
       { rel: 'stylesheet', href: appCss },
       { rel: 'icon', href: '/favicon.ico' },
     ],
